f0a4c5481a2cc47e98dfa92a69b3bbc99d33d6a7
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-04-25 12:38:25 +0100

    Procfile added. Test data added in dev_seeds.js

--- api/dbConfig/dev_seeds.js
+++ api/dbConfig/dev_seeds.js
@@ -1,17 +1,44 @@
 module.exports = [
+
+  //Test data for users
   {
     id: 1,
-    name: 'Clifford',
-    age: 14,
+    user_name: 'Clifford',
+    first_name: 'Me',
+    last_name: 'Too',
+    email: 'fakeemail@gmail.com',
+    habits: [
+      {
+        habit1: 'Placeholder habit',
+
+      }
+
+    ]
   },
   {
     id: 2,
-    name: 'Masha',
-    age: 9,
+    user_name: 'Clifford',
+    first_name: 'Me',
+    last_name: 'Too',
+    email: 'fakeemail@gmail.com',
+    habits: [
+      {
+        habit1: 'Placeholder habit',
+
+      }
+    ]
   },
   {
     id: 3,
-    name: 'Mochi',
-    age: 2,
-  },
+    user_name: 'Clifford',
+    first_name: 'Me',
+    last_name: 'Too',
+    email: 'fakeemail@gmail.com',
+    habits: [
+      {
+        habit1: 'Placeholder habit',
+
+      }
+    ]
+ },
 ];
