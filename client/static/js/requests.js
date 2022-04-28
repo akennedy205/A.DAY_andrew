bd4517c7b199260ddff6ee00eb76e6f78c57ab56
“iAmash412” <aafthab.ashraff@gmail.com>
2022-04-28 02:27:20 +0100

    i have created a fetch function for habits in request.js in clients

--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -16,10 +16,13 @@ async function getAllInfo() {
   }
 }
 
-const form = document.querySelector('form');
-form.addEventListener('submit', postHabit);
+//  Add Habit function
 
-async function postHabit(e) {
+const createHabit = document.getElementById('createHabit');
+
+createHabit.addEventListener('submit', requestHabit);
+
+async function requestHabit(e) {
   e.preventDefault();
   try {
     const options = {
@@ -27,47 +30,71 @@ async function postHabit(e) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
     };
-    const r = await fetch(`http://localhost:3001/${usrname}`, options);
+    console.log(Object.fromEntries(new FormData(e.target)));
+    const r = await fetch(`http://localhost:3001/habits/${username}`, options);
     const data = await r.json();
-    console.log(data);
-    if (data.err) {
-      throw Error(data.err);
+    if (!data.success) {
+      throw new Error('Habit is not valid');
     }
+    login(data.token);
+    window.location.href = 'habitTracker.html';
   } catch (err) {
     console.warn(err);
   }
 }
 
-async function getHabit(username) {
-  try {
-    const response = await fetch(`http://localhost:3001/${username}`);
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.warn(err);
-  }
-}
+// const form = document.querySelector('form');
+// form.addEventListener('submit', postHabit);
 
-const r = await fetch(`http://localhost:3001/${username}`, options);
+// async function postHabit(e) {
+//   e.preventDefault();
+//   try {
+//     const options = {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+//     };
+//     const r = await fetch(`http://localhost:3001/${usrname}`, options);
+//     const data = await r.json();
+//     console.log(data);
+//     if (data.err) {
+//       throw Error(data.err);
+//     }
+//   } catch (err) {
+//     console.warn(err);
+//   }
+// }
 
-async function postHabit(e) {
-  e.preventDefault();
-  try {
-    const options = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.parse(Object.fromEntries(new FormData(e.target))),
-    };
-    const r = await fetch(`http://localhost:3001/habits`, options);
-    const data = await r.json();
-    console.log(data);
-    if (data.err) {
-      throw Error(data.err);
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-}
+// async function getHabit(username) {
+//   try {
+//     const response = await fetch(`http://localhost:3001/${username}`);
+//     const data = await response.json();
+//     return data;
+//   } catch (err) {
+//     console.warn(err);
+//   }
+// }
+
+// const r = await fetch(`http://localhost:3001/${username}`, options);
+
+// async function postHabit(e) {
+//   e.preventDefault();
+//   try {
+//     const options = {
+//       method: 'GET',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.parse(Object.fromEntries(new FormData(e.target))),
+//     };
+//     const r = await fetch(`http://localhost:3001/habits`, options);
+//     const data = await r.json();
+//     console.log(data);
+//     if (data.err) {
+//       throw Error(data.err);
+//     }
+//   } catch (err) {
+//     console.warn(err);
+//   }
+// }
 
 // fetch("http://localhost:3001/users/:id", {
 //   method: "POST",
